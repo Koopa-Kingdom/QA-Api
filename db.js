ce74ea961ef4aa577ad87697fcfdda334535d14a
Nick Amenda <104174057+nickamenda@users.noreply.github.com>
2022-11-02 11:32:14 -0500

    first test query

--- db.js
+++ db.js
@@ -1,3 +1,17 @@
-function test () {
-  console.log(test)
-}
+const path = require("dotenv")
+const { Pool, client } = require('pg')
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT
+})
+
+pool.query('SELECT * FROM answers WHERE answers.question_id = 28', (err, res) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log(res.rows)
+})
